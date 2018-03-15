2ec40b246c75dd8090070d10bf8e5188a3342b8b
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-03-15 21:50:22 +0100

    Update vitrine.html.php

--- vitrine/vitrine.html.php
+++ vitrine/vitrine.html.php
@@ -70,14 +70,14 @@
 				<? echo $test1="PHP marche"; ?><br>
 				<? echo $test2="CA NE MARCHE PAS CA N'A PAS DE JAMBES"; ?>
 			</p>
-			<p>
-				<!--<?
+			<!--<p>
+				<?
 				$dbh = new PDO('mysql:host=54.37.65.115;dbname=docker_project', 'root', 'Lp2407yn');
 				$req = 'SELECT * FROM users;';
 				$rep = $dbh->query($req, PDO::FETCH_ASSOC);
 				echo '<pre>'.$rep.'</pre>';
-				$dbh = null; ?>-->
-			</p>
+				?>
+			</p>-->
 		</main>
 		<footer>
       <p>Groupe Numero 3</p>
