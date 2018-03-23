f667af47929bef217385c1ecd058c84822ff5e58
lolog4m3 <33026810+lolog4m3@users.noreply.github.com>
2018-03-23 16:21:54 +0100

    changement de contenu dans les volumes

---
+++ volume_intranet/intranet.html.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--1TM1 Rosar Nicolas-->
+<html lang=fr>
+	<head>
+		<meta charset="utf-8">
+		<title>Intranet</title>
+		<style>
+
+		body {
+			margin : 0;
+			padding : 0;
+		}
+		footer{
+			//vertical-align: left;
+			text-align:center;
+		}
+		h1, h2, h3{
+			text-decoration:underline;
+		}
+		h1 {
+			text-align : center;
+		}
+		footer{
+			position: fixed;
+			bottom: 0;
+			width:100%;
+			color: white;
+			background-color: blue;
+			border-top : 5px solid black;
+			
+		}
+		p a{
+			border: 1px solid black;
+			margin: 20px;
+			background-color: lightgrey;
+			border-radius: 1em;
+			
+		}
+		a {
+			text-decoration: none;
+			padding: 5px;
+		}
+		</style>
+	</head>
+	<body>
+		<header>
+			<h1>Bienvenue dans l'intranet de WoodyToys</h1><br>
+		</header>
+		<main>
+			<h3>Woodytoys, c'est quoi?</h3>
+			<p>
+			Woodytoys est une entreprise de conception de jeux en bois pour
+			les enfants en bas âges.
+			</p>
+			<br>
+			<h3>Liens vers nos autres sites web :</h3>
+			<p>
+				Site vitrine : 
+				<a href=http://54.37.65.113:81/vitrine.html.php alt="Site vitrine">www.wt3.ephec-ti.be</a>
+			</p>
+			<p>
+				Site business : 
+				<a href=http://54.37.65.113:83/b2B.html.php alt="Site b2B">b2B.wt3.ephec-ti.be</a>
+			</p>
+		</main>
+		
+		<footer>
+			<p><u>
+			Groupe Numero 3 :
+			</u></p>
+			<p>
+			Persyn Loic<br>
+			Rosar Nicolas<br>
+			Vankeerberghen Jordan<br>
+			</p>
+		</footer>
+	</body>
+</html>
