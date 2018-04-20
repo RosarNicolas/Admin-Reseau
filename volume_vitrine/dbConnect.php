da569ce2753493df30aa7f02f7cdc9d664a80b92
lolog4m3 <33026810+lolog4m3@users.noreply.github.com>
2018-04-20 15:17:00 +0200

    Update dbConnect.php

--- volume_vitrine/dbConnect.php
+++ volume_vitrine/dbConnect.php
@@ -12,7 +12,7 @@ catch (Exception $e)
 }
 }
 
-connect('54.37.65.113','nicolas','jeSaisPasQuoi','docker_project');
+connect('54.37.65.115:3306','nicolas','jeSaisPasQuoi','docker_project');
 //54.35.65.115:3306
 
 ?>
