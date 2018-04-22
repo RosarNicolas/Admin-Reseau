9aa4e5651d5b661a707e1df35f566644c2836523
lolog4m3 <33026810+lolog4m3@users.noreply.github.com>
2018-04-22 21:49:49 +0200

    Update dbConnect.php

--- volume_vitrine/dbConnect.php
+++ volume_vitrine/dbConnect.php
@@ -5,6 +5,7 @@ function connect($server,$user,$mdp,$db)
 try
 {
     $bdd = new PDO('mysql:host='.$server.';dbname='.$db.'', $user, $mdp);
+    $bdd->exec('SET NAMES utf8');
     echo print_r("je suis passé");
 }
 catch (Exception $e)
@@ -13,7 +14,8 @@ catch (Exception $e)
 }
 }
 
-connect('172.17.0.6','Nicolas','jeSaisPasQuoi','docker_project');
+connect('172.17.0.6','nicolas','jeSaisPasQuoi','docker_project');
+//172.17.0.4 adresse container VPS à la place de .6 qui est mySQL 
 
 
 ?>
