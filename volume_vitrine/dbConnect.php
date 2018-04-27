b54e4ccddff5766f84b4dbc402e94c72e4dffaeb
jordanvkb201460 <33064840+jordanvkb201460@users.noreply.github.com>
2018-04-27 15:25:47 +0200

    Update dbConnect.php

--- volume_vitrine/dbConnect.php
+++ volume_vitrine/dbConnect.php
@@ -9,7 +9,7 @@ try
     echo print_r("je suis passé");
     $req = 'USE admini;SELECT * FROM user;';
 	$rep = $dbh->query($req, PDO::FETCH_ASSOC);
-	echo print_r(($rep->fetchAll(),1));
+	echo print_r($rep->fetchAll(),1);
 }
 catch (Exception $e)
 {
