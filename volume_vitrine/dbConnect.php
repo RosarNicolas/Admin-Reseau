45cd50459e9d0b550ba14e958a8582fb28829c54
jordanvkb201460 <33064840+jordanvkb201460@users.noreply.github.com>
2018-04-27 14:51:57 +0200

    Update dbConnect.php

--- volume_vitrine/dbConnect.php
+++ volume_vitrine/dbConnect.php
@@ -7,6 +7,10 @@ try
     $bdd = new PDO('mysql:host='.$server.';dbname='.$db.'', $user, $mdp);
     $bdd->exec('SET NAMES utf8');
     echo print_r("je suis passé");
+    $req = 'USE admini;SELECT * FROM user;';
+	$rep = $dbh->query($req, PDO::FETCH_ASSOC);
+	($rep->fetchAll(),1);
+	return $rep;
 }
 catch (Exception $e)
 {
@@ -14,7 +18,7 @@ catch (Exception $e)
 }
 }
 
-connect('172.17.0.6','nicolas','jeSaisPasQuoi','docker_project');
+//connect('172.17.0.6','nicolas','jeSaisPasQuoi','docker_project');
 //172.17.0.4 adresse container VPS à la place de .6 qui est mySQL 
 
 
