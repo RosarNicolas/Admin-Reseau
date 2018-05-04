860ac8dd53e37af1fc049f948314e59545340e96
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-05-04 16:41:38 +0200

    Create index.php

---
+++ volume_b2b/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--1TM1 Rosar Nicolas-->
+<html lang=fr>
+	<head>
+		<meta charset="utf-8">
+		<title>Intranet</title>
+		<style>
+
+		body {
+			margin : 0;
+			padding : 0;
+		}
+		footer{
+			//vertical-align: left;
+			text-align:center;
+		}
+		h1, h2, h3{
+			text-decoration:underline;
+		}
+		h1 {
+			text-align : center;
+		}
+		footer{
+			position: fixed;
+			bottom: 0;
+			width:100%;
+			color: white;
+			background-color: blue;
+			border-top : 5px solid black;
+			
+		}
+		p a{
+			border: 1px solid black;
+			margin: 20px;
+			background-color: lightgrey;
+			border-radius: 1em;
+			
+		}
+		a {
+			text-decoration: none;
+			padding: 5px;
+		}
+		</style>
+	</head>
+	<body>
+		<header>
+			<h1>Bienvenue dans le b2b de WoodyToys</h1><br>
+		</header>
+		
+		<footer>
+			<p><u>
+			Groupe Numero 3 :
+			</u></p>
+			<p>
+			Persyn Loic<br>
+			Rosar Nicolas<br>
+			Vankeerberghen Jordan<br>
+			</p>
+		</footer>
+	</body>
+</html>
