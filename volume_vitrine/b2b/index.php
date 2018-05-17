4e29e8f1261ca3b49229393d90ff0aa2325439de
RosarNicolas2TL1 <32072799+RosarNicolas2TL1@users.noreply.github.com>
2018-05-17 14:52:53 +0200

    Update index.php

--- volume_vitrine/b2b/index.php
+++ volume_vitrine/b2b/index.php
@@ -76,12 +76,17 @@ $sqlSelect = <<<'EOD'
 SELECT *
 FROM user
 EOD;
+				try{
+				
 				$dbh = new PDO("mysql:host=172.17.0.3;dbname=admini", "nicolas","motdepasse");
 				$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 				$re = $dbh->query($sqlSelect, PDO::FETCH_ASSOC);
 				echo print_r($rq,1);
 				echo 1;
-				
+				}
+				catch(PDOException $e){
+					die $e->getMessage();
+				}
 				
 				
 				
